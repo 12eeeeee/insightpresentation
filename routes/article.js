a51eb7ea6976197020999e74d3329605a4d073e6
12eeeeee <ig2.cij@gmail.com>
2014-02-20 10:24:16 +0900

    02201023

--- routes/article.js
+++ routes/article.js
@@ -50,23 +50,28 @@ module.exports = function (app) {
         });
 
     app.get('/Pmenu/:cPage', function(req, res){
+<<<<<<< HEAD
       var currentPage = req.param('cPage');
+=======
+      var cPage = req.param('cPage');
+>>>>>>> 02200914 morning update
         res.render('pmenu',
             {
-                 user : req.user,  Article : req.Article , cPage : currentPage    
+                 user : req.user,  Article : req.Article , cPage : req.param('cPage')    
             });
 
-        console.log(currentPage);
+        console.log(cPage);
 
-        Article.find({ ptname : currentPage }, function(err, Articles, count){
+        Article.find({ ptname : cPage }, function(err, Articles, count){
             console.log(Articles); //현재 페이지 제대로 들어 왔는지 확인 
 
 
         });
 
-        res.send(currentPage);
+        res.send(cPage);
     });
 
+<<<<<<< HEAD
     app.post('/canvas', function(req, res){
         console.log('canvas is made');
 
@@ -76,6 +81,20 @@ module.exports = function (app) {
 
     app.get('/canvas', function(req, res){
         res.render('canvas', {user : req.user , Article : req.Article, cPage : req.cPage});
+=======
+    app.post('/canvas/:cPage', function(req, res){
+        var cPage = req.param('cPage');
+        console.log('canvas is made');
+        console.log('cPage'+cPage);
+        console.log('JLIST'+req.body.JLIST);
+        res.redirect('/Pmenu/:cPage');
+
+    });
+
+    app.get('/canvas/:cPage', function(req, res){
+        var cPage = req.param('cPage');
+        res.render('canvas', {user : req.user , Article : req.Article, cPage : cPage});
+>>>>>>> 02200914 morning update
 
     });
    //res.render('Plist', {user : req.user, Article : Article});
